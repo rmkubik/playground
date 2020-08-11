4983ee2c76c926e257435a4068a253c293aaaf82
Ryan Kubik <rkubik@twilio.com>
2020-08-11 10:57:05 -0700

    Pixi POC

---
+++ pixi-test/index.js
@@ -0,0 +1,34 @@
+import * as PIXI from "pixi.js";
+import fogOwlImage from "./fog_owl.gif";
+
+// The application will create a renderer using WebGL, if possible,
+// with a fallback to a canvas render. It will also setup the ticker
+// and the root stage PIXI.Container.
+const app = new PIXI.Application();
+
+// The application will create a canvas element for you that you
+// can then insert into the DOM.
+document.body.appendChild(app.view);
+
+// load the texture we need
+app.loader.add("fogOwl", fogOwlImage).load((loader, resources) => {
+  // This creates a texture from a 'bunny.png' image.
+  const fogOwl = new PIXI.Sprite(resources.fogOwl.texture);
+
+  // Setup the position of the bunny
+  fogOwl.x = app.renderer.width / 2;
+  fogOwl.y = app.renderer.height / 2;
+
+  // Rotate around the center
+  fogOwl.anchor.x = 0.5;
+  fogOwl.anchor.y = 0.5;
+
+  // Add the bunny to the scene we are building.
+  app.stage.addChild(fogOwl);
+
+  // Listen for frame updates
+  app.ticker.add(() => {
+    // each frame we spin the bunny around a bit
+    fogOwl.rotation += 0.01;
+  });
+});
