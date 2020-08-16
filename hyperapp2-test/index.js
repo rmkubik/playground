dd5d61c09104e46992c1ff33a91524c0f82bd2ef
Ryan Kubik <rmkubik@me.com>
2020-08-15 20:27:21 -0700

    Create a db + label

--- hyperapp2-test/index.js
+++ hyperapp2-test/index.js
@@ -2,6 +2,8 @@ import { app } from "hyperapp";
 import h from "./hyperapp-jsx";
 
 import titleTxt from "./assets/title.txt";
+import tiles from "./assets/tiles.png";
+
 import "./main.scss";
 
 const onDomEvent = (eventType) =>
@@ -31,6 +33,43 @@ const Click = (state, event) => {
   }
 };
 
+// const SpriteContainer = styled.div`
+//   width: ${(props) => props.width * props.scale}px;
+//   height: ${(props) => props.height * props.scale}px;
+//   background-color: ${(props) => props.theme.palette[props.background]};
+// `;
+
+// const Sprite = styled.div`
+//   background: url(${(props) => props.sheet}) -${(props) =>
+//       props.col * props.width}px -${(props) => props.row * props.height}px;
+//   image-rendering: pixelated;
+//   width: ${(props) => props.width}px;
+//   height: ${(props) => props.height}px;
+//   transform: scale(${(props) => props.scale});
+//   transform-origin: top left;
+// `;
+
+const Sprite = ({ sheet, row, col, width, height, scale = 1 }) => {
+  return (
+    <div
+      style={{
+        width: `${width * scale}px`,
+        height: `${height * scale}px`,
+      }}
+    >
+      <div
+        style={{
+          background: `url(${sheet}) -${col * width}px -${row * height}px`,
+          width: `${width}px`,
+          height: `${height}px`,
+          transform: `scale(${scale})`,
+          transformOrigin: "top left",
+        }}
+      ></div>
+    </div>
+  );
+};
+
 const Main = () => {
   return (
     <main>
@@ -47,6 +86,11 @@ const Map = () => {
   return (
     <main>
       <p>Map View</p>
+      <Sprite sheet={tiles} row={0} col={0} width={16} height={16} scale={3} />
+      <p>127.0.0.1</p>
+      <p>
+        <span style={{ color: "green" }}>● </span> 200
+      </p>
     </main>
   );
 };
